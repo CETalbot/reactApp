8e713c4f9fcdef5a43e31411de9b3896b9c29890
Catherine <ctalbot5@uclan.ac.uk>
2018-05-11 16:51:47 +0100

    finished project

--- src/box.js
+++ src/box.js
@@ -12,21 +12,38 @@ class Box extends Component {
 		this.new = this.new.bind(this)
 		this.renderNote = this.renderNote.bind(this)
 		this.renderDisplay = this.renderDisplay.bind(this)
+
 	}
 	
 	save(){
-		this.setState({
-			uploading: true
-		})
+		var reader = new FileReader()
+		var fileimg = document.getElementById('imgUpload').files[0]
+		reader.onloadend = () => {
+			this.setState({
+				uploading: true,
+				message: document.getElementById("description").value,
+				file: fileimg,
+
+				imgPreview: reader.result
+			})
+		
+		
+		}
+		reader.readAsDataURL(fileimg)
 	}
 	new(){
+		this.setState({
+			uploading: false
+		})
 	}
 	renderNote(){
 		return(
 			<div className="note">
-				<p>Hi</p>
+				<h1 id="photoTitle">Your Photos</h1>
+				<img src={this.state.imgPreview} alt="uploading image"/>
+				<p>{this.state.message}</p>
 				<span>
-					<button id="new" onClick={this.save}>Add another image</button>
+					<button id="new" onClick={this.new}>Add another image</button>
 				</span>
 			</div>
 		)
@@ -36,7 +53,7 @@ class Box extends Component {
 			<div className="box">
 				<h1 id="title"> Adding a new Image </h1>
 				<p>Add Image</p>
-				<textarea id="image" />
+				<input id="imgUpload" type="file" />
 				<p>Add Description</p>
 				<textarea id="description" ref={input => this._newText = input}/>
 				<span>
